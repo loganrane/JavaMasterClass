cff732440dc336c2e6597bfd45596d18ea4f8182
Arpit <arpitfalcon1@gmail.com>
2020-04-28 16:29:41 +0530

    Multiple Catch Blocks to handle multiple exceptions.

---
+++ Levelling/src/Videos/MultipleCatchBlocks.java
@@ -0,0 +1,19 @@
+package Videos;
+
+public class MultipleCatchBlocks {
+    public static void main(String[] args){
+        int[] number = {4, 8, 16, 32, 64, 128, 256, 512};
+        int[] denom = {2, 0, 4, 4, 0, 8};
+        for(int i=0; i<number.length; i++){
+            try{
+                System.out.println(number[i] + " / " + denom[i] + " is " + number[i] / denom[i]);
+            }
+            catch(ArithmeticException e){
+                System.out.println("Can't divide by zero.");
+            }
+            catch(ArrayIndexOutOfBoundsException e){
+                System.out.println("No matching element found");
+            }
+        }
+    }
+}
