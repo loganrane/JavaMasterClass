3f5f42bfeab43a07963c2bcdfb4196bd45f93840
Arpit <arpitfalcon1@gmail.com>
2020-04-30 21:45:42 +0530

    Running Multiple Threads

--- Mastering/src/Threads/RunningMultipleThreads.java
+++ Mastering/src/Threads/RunningMultipleThreads.java
@@ -10,20 +10,36 @@ public class RunningMultipleThreads {
         MyThread1 myThread3 = new MyThread1("Delta");
         MyThread1 myThread4 = new MyThread1("Gamma");
 
+        // Thread Priority
+        myThread.thread.setPriority(Thread.MIN_PRIORITY);
+        System.out.println("Alpha priority is " + myThread.thread.getPriority());
+        myThread3.thread.setPriority(Thread.MAX_PRIORITY);
+        System.out.println("Delta priority is " + myThread3.thread.getPriority());
+
+        //  Do while loop to print main thread function.
+        do {
+//            System.out.println(".");
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                System.out.println("Main thread interrupted.");
+                e.printStackTrace();
+            }
+        } while(myThread.thread.isAlive() || myThread2.thread.isAlive() || myThread3.thread.isAlive());
 
         // Construct an actual thread from our previous object.
         // Thread newThread = new Thread(myThread);
         // newThread.start();
 
-        for(int i=0; i< 50; i++){
-            System.out.print(".");
-            try {
-                Thread.sleep(100);
-            } catch (InterruptedException e) {
-                System.out.println("Main thread interrupted.");
-                e.printStackTrace();
-            }
-        }
+//        for(int i=0; i< 50; i++){
+//            System.out.print(".");
+//            try {
+//                Thread.sleep(100);
+//            } catch (InterruptedException e) {
+//                System.out.println("Main thread interrupted.");
+//                e.printStackTrace();
+//            }
+//        }
         System.out.println("Main thread ending.");
     }
 }
@@ -36,7 +52,7 @@ class MyThread1 implements Runnable{
         for(int count = 0; count < 10; count++){
             try {
                 // Creating a sleep for 400 ms.
-                Thread.sleep(500);
+                Thread.sleep(400);
                 System.out.println("In " + myTread + " count is " + count);
             } catch (InterruptedException e) {
                 System.out.println(myTread + " interrupted.");
@@ -52,6 +68,5 @@ class MyThread1 implements Runnable{
         // Starting our thread in constructor now.
         thread = new Thread(this, myTread);
         thread.start();
-
     }
 }
