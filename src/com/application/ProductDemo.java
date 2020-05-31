d4277d4931f4aa7e66d5c58496a330c76e52aa04
Arpit <arpitfalcon1@gmail.com>
2020-05-31 16:03:52 +0530

    Starting out with Question 4. Product Class made.

---
+++ src/com/application/ProductDemo.java
@@ -0,0 +1,4 @@
+package com.application;
+
+public class ProductDemo {
+}
---
+++ src/com/product/Product.java
@@ -0,0 +1,31 @@
+package com.product;
+
+public class Product {
+    private int prodNo;
+    private String prodName;
+    private double prodPrice;
+
+    public int getProdNo() {
+        return prodNo;
+    }
+
+    public void setProdNo(int prodNo) {
+        this.prodNo = prodNo;
+    }
+
+    public String getProdName() {
+        return prodName;
+    }
+
+    public void setProdName(String prodName) {
+        this.prodName = prodName;
+    }
+
+    public double getProdPrice() {
+        return prodPrice;
+    }
+
+    public void setProdPrice(double prodPrice) {
+        this.prodPrice = prodPrice;
+    }
+}
